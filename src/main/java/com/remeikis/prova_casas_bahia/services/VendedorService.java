1150faedaa6d084849c584ae4f4eec78ace04a0c
= <brunocoutinhoremeikis@gmail.com>
2024-08-05 12:56:18 -0300

    Valida identificadores (CPF e CNPJ)

--- src/main/java/com/remeikis/prova_casas_bahia/services/VendedorService.java
+++ src/main/java/com/remeikis/prova_casas_bahia/services/VendedorService.java
@@ -3,7 +3,9 @@ package com.remeikis.prova_casas_bahia.services;
 import com.remeikis.prova_casas_bahia.models.Filial;
 import com.remeikis.prova_casas_bahia.models.Vendedor;
 import com.remeikis.prova_casas_bahia.models.dto.CreateVendedorDto;
+import com.remeikis.prova_casas_bahia.models.enums.TipoContratacao;
 import com.remeikis.prova_casas_bahia.repositories.VendedorRepository;
+import com.remeikis.prova_casas_bahia.utils.IdentifierUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +34,8 @@ public class VendedorService {
     public Vendedor create(CreateVendedorDto vendedorDto) {
         Filial filial = validateFilial(vendedorDto.getIdFilial());
 
+        validateIdentificador(vendedorDto.getIdentificador(), vendedorDto.getTipoContratacao());
+
         Vendedor vendedor = new Vendedor(
             null,
             vendedorDto.getNome(),
@@ -48,6 +52,8 @@ public class VendedorService {
 
     public Vendedor update(Vendedor vendedor) {
         Filial filial = validateFilial(vendedor.getIdFilial());
+
+        validateIdentificador(vendedor.getIdentificador(), vendedor.getTipoContratacao());
 
         if(!exists(vendedor)) {
             throw new IllegalArgumentException("Não existe vendedor com matrícula '" + vendedor.getMatricula() + "'");
@@ -77,5 +83,14 @@ public class VendedorService {
             throw new IllegalArgumentException("Não existe filial com ID " + idFilial);
         }
         return filial;
+    }
+
+    private void validateIdentificador(String identificador, TipoContratacao tipoContratacao) throws IllegalArgumentException {
+        if (tipoContratacao.equals(TipoContratacao.PJ)) {
+           if (!IdentifierUtil.isCnpj(identificador))
+               throw new IllegalArgumentException("O tipo de contratação " + tipoContratacao.name() + " requer um CNPJ como identificador. '" + identificador + "' não é um CNPJ válido.");
+        }
+        else if (!IdentifierUtil.isCpf(identificador))
+            throw new IllegalArgumentException("O tipo de contratação " + tipoContratacao.name() + " requer um CPF como identificador. '" + identificador + "' não é um CPF válido.");
     }
 }
